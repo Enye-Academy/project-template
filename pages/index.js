5d48ee17c139415767bb596e2ab0aa65e0a3038d
Justice <jotuya2@gmail.com>
2019-04-21 00:38:44 +0100

    integrated tailwind and antdesign

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import '../src/index.css';
+
+// const test = () => <div className="text-orange-dark hover:text-red">Today is my cay</div>;
+
+// export default test;
