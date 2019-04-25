501acefab3c12041e85b4d346969efe2e42c86fb
Justice <jotuya2@gmail.com>
2019-04-25 08:23:38 +0100

    fixed Landing page Bug reported by travis ci

--- components/LandingPage/components/LandingPage.test.jsx
+++ components/LandingPage/components/LandingPage.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LandingPage from './LandingPage';
+import LandingPage from './index';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
--- components/LandingPage/components/index.js
+++ components/LandingPage/components/index.js
@@ -1,3 +1,4 @@
+
 import LandingPage from './LandingPage';
 
 export default LandingPage;
