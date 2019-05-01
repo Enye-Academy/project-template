1ab7093382ddf42d48af59323040ebdd1bf77414
Ucheya <chimereucheya.okereke@shuttlers.ng>
2019-05-01 11:37:53 +0100

    Add babelrc file and setup. Add webpack file and config settings for both dev and prod application. Modify eslintrc rules. Setup the general client side and server side structure. Included basic files in structure to enable the team know the exact file convention to follow.

---
+++ client/js/app/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Router } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
+import history from '../utils/history';
+
+const App = () => (
+  <Router history={history}>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        name="landing"
+        // component={(Home)}
+      />
+    </Switch>
+  </Router>
+);
+export default App;
---
+++ server/config/database.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+  url: process.env.MONGODB_DEV_URL,
+  url_production: process.env.MONGODB_URL,
+  url_test: process.env.MONGODB_TEST_URL,
+};
