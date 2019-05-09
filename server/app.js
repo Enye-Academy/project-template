7384214a54e48489c100733160b0de19df63ba3b
JediBoy <faithomojola@gmail.com>
2019-05-09 16:26:26 +0100

    application setup update

--- server/app.js
+++ server/app.js
@@ -1,3 +1,9 @@
+/* eslint no-restricted-globals: 0 */
+/* eslint indent: 0 */
+/* eslint no-tabs: 0 */
+/* eslint no-unused-vars: 0 */
+/* eslint no-mixed-spaces-and-tabs: 0 */
+/* eslint arrow-body-style:0 */
 import express from 'express';
 import mongoose from 'mongoose';
 import expressValidator from 'express-validator';
@@ -9,59 +15,58 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import next from 'next';
 import routes from './routes';
 import devConfig from '../webpack.config.babel';
+import configDB from './config/database';
 
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = parseInt(process.env.PORT, 10) || 3000;
+const server = express(); // Set up the express app
 dotenv.config();
-
-const port = parseInt(process.env.PORT, 10) || 8000;
-
-// database config
-const configDB = require('./config/database');
-
-// Set up the express app
-const app = express();
-
-
 mongoose.Promise = global.Promise;
 let compiler;
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 if (process.env.NODE_ENV === 'production') {
-  mongoose.connect(configDB.url_production, { useNewUrlParser: true });
+	mongoose.connect(configDB.url_production, { useNewUrlParser: true });
 } else if (process.env.NODE_ENV === 'test') {
-  mongoose.connect(configDB.url_test, { useNewUrlParser: true });
-  compiler = webpack(devConfig);
+	mongoose.connect(configDB.url_test, { useNewUrlParser: true });
+  	compiler = webpack(devConfig);
 } else {
-  mongoose.connect(configDB.url, { useNewUrlParser: true });
-  compiler = webpack(devConfig);
+  	mongoose.connect(configDB.url, { useNewUrlParser: true });
+  	compiler = webpack(devConfig);
 }
+
 // Log requests to the console.
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
-app.use(express.static(__dirname + './../public'));
-app.use('/api-docs', express.static(path.join(__dirname, './../api-docs')));
+server.use(logger('dev'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(expressValidator());
+server.use(express.static(path.join(__dirname, './../public')));
+server.use('/api-docs', express.static(path.join(__dirname, './../api-docs')));
 
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-  app.use(webpackDevMiddleware(compiler, {
-    publicPath: devConfig.output.publicPath,
-    open: false
-  }));
-  app.use(webpackHotMiddleware(compiler));
+	server.use(webpackDevMiddleware(compiler, {
+		open: false,
+	  	publicPath: devConfig.output.publicPath,
+  	}));
+  	server.use(webpackHotMiddleware(compiler));
 }
-app.use('/api/v1', routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-});
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err, 'but stuff works'); //eslint-disable-line
-  } else {
-    console.log(`We are live on port ${port}...`.green); //eslint-disable-line
-  }
-});
+app.prepare().then(() => {
+	server.get('*', (req, res) => {
+		return handle(req, res);
+    	// res.sendFile(path.join(__dirname, '../client/index.html'));
+	});
 
-module.exports = app;
+	server.listen(port, err => {
+		if (err) throw err;
+		console.log(`> Ready on http://localhost:${port}`); /* eslint no-console:0 */
+	});
+}).catch(ex => {
+	console.error(ex); /* eslint no-console:0 */
+    process.exit(1);
+});
