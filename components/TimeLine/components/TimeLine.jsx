862d057f690a0590b3e98cdfb33718f44790ecd5
Justice Otuya <jotuya2@gmail.com>
2019-05-29 08:58:37 +0100

    minor fix

--- components/TimeLine/components/TimeLine.jsx
+++ components/TimeLine/components/TimeLine.jsx
@@ -17,7 +17,7 @@ import TimeLinePopularTopic from './TimeLinePopularTopic';
 import TimeLineOnlineFriends from './TimeLineOnlineFriends';
 import TimeLinePosts from './TimeLinePosts';
 
-/**Helper function that is used to render the TimeLine Component
+/** Helper function that is used to render the TimeLine Component
  * @class TimeLine
  * @extends {React.Component}
  * @return {Object} returns the TimeLine component
@@ -34,12 +34,12 @@ class TimeLine extends React.Component {
             activeComment: '',
             activeLikeButton: '',
             statusValue: '',
-            commentValue:''
+            commentValue: '',
         };
         this.handleOk = this.handleOk.bind(this);
         this.handleLikeButton = this.handleLikeButton.bind(this);
         this.handleStatusValue = this.handleStatusValue.bind(this);
-        this.handleCommentValue =  this.handleCommentValue.bind(this);
+        this.handleCommentValue = this.handleCommentValue.bind(this);
     }
 
     componentDidMount() {
@@ -51,7 +51,6 @@ class TimeLine extends React.Component {
      * @function
      * @return {Object} returns 'true' to show the modal
      */
-
     modalHandler = () => {
         this.setState({
             visible: !this.state.visible,
@@ -64,13 +63,12 @@ class TimeLine extends React.Component {
      * @return {Object} returns 'false' to close the modal post component
      */
     handleOk = e => {
-        const {visible} = this.state;
+        const { visible } = this.state;
         // close the modal;
-        if (visible){
+        if (visible) {
             this.setState({
-                visible: false
-            })
-            console.log('handle ok', this.state.status)
+                visible: false,
+            });
         }
 
         // make an api call
@@ -95,28 +93,28 @@ class TimeLine extends React.Component {
         });
     };
 
-      handleCommentValue = e => {
+    handleCommentValue = e => {
         this.setState({
-            commentValue:e.target.value
-        })
-        console.log(e.target.value)
+            commentValue: e.target.value,
+        });
     }
 
     handleStatusValue = e => {
         this.setState({
-            statusValue:e.target.value
-        })
-        console.log(e.target.value)
+            statusValue: e.target.value,
+        });
     }
 
     render() {
-        const { profileData } = this.state;
+        const {
+            profileData, visible, statusValue, like, likeCount, activeComment, activeLikeButton, commentValue,
+        } = this.state;
         return (
             <PageLayout
-                isSiderPresent={profileData.length > 0}
-                isFooterPresent={false}
-                isAuthenticated
-                title={TIMELINE_TITLE}
+              isSiderPresent={profileData.length > 0}
+              isFooterPresent={false}
+              isAuthenticated
+              title={TIMELINE_TITLE}
             >
                 <main className="TimeLine_content">
 
@@ -127,11 +125,11 @@ class TimeLine extends React.Component {
                         </div>
 
                         <CreatePostModal
-                            visible={this.state.visible}
-                            handleOkFunction={this.handleOk}
-                            closeModal={this.modalHandler}
-                            handleOnChange={this.handleStatusValue}
-                            textValue={this.state.statusValue}
+                          visible={visible}
+                          handleOkFunction={this.handleOk}
+                          closeModal={this.modalHandler}
+                          handleOnChange={this.handleStatusValue}
+                          textValue={statusValue}
                         />
                     </section>
 
@@ -147,30 +145,29 @@ class TimeLine extends React.Component {
                         {/* create post component */}
                         <section className="TimeLine-post-component">
                             <CreatePostComponent
-                                InputPlaceholder={CREATEPOST_PLACEHOLDER}
-                                rowHeight={5}
-                                handleOnChange={this.handleStatusValue}
-                                textValue={this.state.statusValue}
-                                
+                              InputPlaceholder={CREATEPOST_PLACEHOLDER}
+                              rowHeight={5}
+                              handleOnChange={this.handleStatusValue}
+                              textValue={statusValue}
                             />
                         </section>
 
                         <Divider />
 
-<section style={{background:'white'}}>
-                        {/* timeline posts */}
-                        <TimeLinePosts
-                            profileData={profileData}
-                            like={this.state.like}
-                            likeCount={this.state.likeCount}
-                            activeComment={this.state.activeComment}
-                            activeLikeButton={this.state.activeLikeButton}
-                            handleComment={this.handleComment}
-                            handleLikeButton={this.handleLikeButton}
-                            handleOk={this.handleOk}
-                                handleOnChange={this.handleCommentValue}
-                                textValue={this.state.commentValue}
-                        />
+                        <section style={{ background: 'white' }}>
+                            {/* timeline posts */}
+                            <TimeLinePosts
+                              profileData={profileData}
+                              like={like}
+                              likeCount={likeCount}
+                              activeComment={activeComment}
+                              activeLikeButton={activeLikeButton}
+                              handleComment={this.handleComment}
+                              handleLikeButton={this.handleLikeButton}
+                              handleOk={this.handleOk}
+                              handleOnChange={this.handleCommentValue}
+                              textValue={commentValue}
+                            />
                         </section>
                     </section>
                 </main>
